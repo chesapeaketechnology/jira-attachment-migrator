b231d1efb51933f6882233486d246b61c99f73cd
Eric Valenzuela <evalenzuela@ctic.us>
2021-06-07 14:27:56 -0700

    TGA-43: combined both scripts

--- app/index.js
+++ app/index.js
@@ -1,13 +1,14 @@
 const settings = require("../settings.json");
-const JiraApi = require("./jiraApi.js");
-const TM4JApi = require("./tm4jApi.js");
-const fs = require('fs');
+const SourceJiraApi = require("./sourceJiraApi.js");
+const TargetJiraApi = require("./targetJiraApi.js");
+const TargetTM4JApi = require("./targetTm4jApi.js");
+const fs = require("fs");
 
 function deleteFolderRecursive(path) {
-  if( fs.existsSync(path) ) {
-    fs.readdirSync(path).forEach(function(file, index) {
+  if (fs.existsSync(path)) {
+    fs.readdirSync(path).forEach(function (file, index) {
       var curPath = path + "/" + file;
-      if(fs.lstatSync(curPath).isDirectory()) {
+      if (fs.lstatSync(curPath).isDirectory()) {
         deleteFolderRecursive(curPath);
       } else {
         fs.unlinkSync(curPath);
@@ -15,51 +16,111 @@ function deleteFolderRecursive(path) {
     });
     fs.rmdirSync(path);
   }
-};
+}
+
+async function migrateJira() {
+  var sourceJiraApi = new SourceJiraApi(settings.sourceJira);
+  var targetJiraApi = new TargetJiraApi(settings.targetJira);
+
+  console.log("Validating connection to source Jira...");
+  if (!(await sourceJiraApi.validate())) {
+    console.log(
+      "Error trying to connect to the source Jira instance. Aborting."
+    );
+    return;
+  }
+
+  console.log("Validating connection to target Jira...");
+  if (!(await targetJiraApi.validate())) {
+    console.log(
+      "Error trying to connect to the target Jira instance. Aborting."
+    );
+    return;
+  }
+
+  console.log("Creating directory to store downloaded attachments...");
+  deleteFolderRecursive("./attachments");
+  fs.mkdirSync("./attachments");
+
+  console.log("All good. Ready to start Jira migration...\n");
+
+  var issue;
+  let index = 1;
+  while ((issue = await targetJiraApi.getNextTestCase())) {
+    console.log(`#${index++} Issue ${issue.key}:`);
+    var issueKey = issue.key;
+    if (!issueKey) {
+      console.log("\tNo issue key has been found. Skipping...");
+      continue;
+    }
+    var attachments = await sourceJiraApi.getAttachments(issueKey);
+    if (!attachments.length) {
+      console.log(`\tNo attachments to upload from issue ${issueKey}.`);
+      continue;
+    }
+    console.log(
+      `\tFound ${attachments.length} attachments. Downloading attachments from issue ${issueKey}...`
+    );
+    await sourceJiraApi.downloadAttachments(issueKey, attachments);
+    console.log(`\tUploading attachments to issue ${issue.key}...`);
+    await targetJiraApi.uploadAttachments(issueKey, issue.key);
+  }
+  console.log("Jira migration complete!");
+}
+
+async function migrateTm4j() {
+  var sourceJiraApi = new SourceJiraApi(settings.sourceJira);
+  var targetTm4jApi = new TargetTM4JApi(settings.targetJira);
+
+  console.log("Validating connection to source Jira...");
+  if (!(await sourceJiraApi.validate())) {
+    console.log(
+      "Error trying to connect to the source Jira instance. Aborting."
+    );
+    return;
+  }
+
+  console.log("Validating connection to target Jira...");
+  if (!(await targetTm4jApi.validate())) {
+    console.log(
+      "Error trying to connect to the target Jira instance. Aborting."
+    );
+    return;
+  }
+
+  console.log("Creating directory to store downloaded attachments...");
+  deleteFolderRecursive("./attachments");
+  fs.mkdirSync("./attachments");
+
+  console.log("All good. Ready to start TM4J migration...\n");
+
+  var testCase;
+  let index = 1;
+  while ((testCase = await targetTm4jApi.getNextTestCase())) {
+    console.log(`#${index++} Test case ${testCase.key}:`);
+    var issueKey = targetTm4jApi.getIssueKey(testCase);
+    if (!issueKey) {
+      console.log("\tNo issue key has been found. Skipping...");
+      continue;
+    }
+    var attachments = await sourceJiraApi.getAttachments(issueKey);
+    if (!attachments.length) {
+      console.log(`\tNo attachments to upload from issue ${issueKey}.`);
+      continue;
+    }
+    console.log(
+      `\tFound ${attachments.length} attachments. Downloading attachments from issue ${issueKey}...`
+    );
+    await sourceJiraApi.downloadAttachments(issueKey, attachments);
+    console.log(`\tUploading attachments to test case ${testCase.key}...`);
+    await targetTm4jApi.uploadAttachments(issueKey, testCase.key);
+  }
+  console.log("TM4J migration complete!");
+}
 
 async function migrate() {
-	var jiraApi = new JiraApi(settings.sourceJira);
-	var tm4jApi = new TM4JApi(settings.targetJira);
-
-	console.log('Validating connection to source Jira...');
-	if(!(await jiraApi.validate())) {
-		console.log('Error trying to connect to the source Jira instance. Aborting.');
-		return;
-	}
-
-	console.log('Validating connection to target Jira...');
-	if(!(await tm4jApi.validate())) {
-		console.log('Error trying to connect to the target Jira instance. Aborting.');
-		return;
-	}
-	
-	console.log('Creating directory to store downloaded attachments...');
-	deleteFolderRecursive('./attachments');
-	fs.mkdirSync('./attachments');
-
-	console.log('All good. Ready to start...\n');
-
-	var testCase;
-
-	let index = 1;
-	while(testCase = await tm4jApi.getNextTestCase()) {
-		console.log(`#${index++} Test case ${testCase.key}:`);
-		var issueKey = tm4jApi.getIssueKey(testCase);
-		if(!issueKey) {
-			console.log('\tNo issue key has been found. Skipping...');
-			continue;
-		}
-		var attachments = await jiraApi.getAttachments(issueKey);
-		if(!attachments.length) {
-			console.log(`\tNo attachments to upload from issue ${issueKey}.`);
-			continue;	
-		}
-		console.log(`\tFound ${attachments.length} attachments. Downloading attachments from issue ${issueKey}...`);
-		await jiraApi.downloadAttachments(issueKey, attachments);
-		console.log(`\tUploading attachments to test case ${testCase.key}...`);
-		await tm4jApi.uploadAttachments(issueKey, testCase.key);
-	}
-	console.log('Migration complete!');
+  await migrateJira();
+  await migrateTm4j();
 }
 
 migrate();
---
+++ app/targetJiraApi.js
@@ -0,0 +1,131 @@
+const fetch = require("node-fetch");
+const HttpUtils = require("./httpUtils.js");
+const fs = require("fs");
+const FormData = require("form-data");
+
+class TargetJiraApi {
+  constructor(jiraSettings) {
+    this.jiraSettings = jiraSettings;
+    this.currentTestCaseIndex = 0;
+    this.currentPage = 0;
+    this.firstPageLoaded = false;
+    this.pageSize = 200;
+  }
+
+  getIssueKey(testCase) {
+    if (!testCase.customFields) {
+      return;
+    }
+    return testCase.customFields[this.jiraSettings.issueKeyCustomField];
+  }
+
+  async validate() {
+    const response = await fetch(
+      this._getMyselfUrl(),
+      HttpUtils.getAuthHeader(
+        this.jiraSettings.user,
+        this.jiraSettings.password
+      )
+    );
+    const isValid = response.status == 200;
+    if (!isValid) {
+      console.log(await response.text());
+    }
+    return isValid;
+  }
+
+  async getNextTestCase() {
+    if (!this.testCases) {
+      await this._loadNextTestCasePage();
+    }
+
+    if (this.currentTestCaseIndex >= this.testCases.length) {
+      let isEof = await this._loadNextTestCasePage();
+      if (isEof) {
+        return;
+      }
+      this.currentTestCaseIndex = 0;
+    }
+
+    const testCase = this.testCases[this.currentTestCaseIndex];
+    this.currentTestCaseIndex++;
+    return testCase;
+  }
+
+  async uploadAttachments(issueKey) {
+    const attachmentsDir = `./attachments/${issueKey}`;
+    const files = fs.readdirSync(attachmentsDir);
+    const formData = new FormData();
+
+    for (let file of files) {
+      var filePath = `${attachmentsDir}/${file}`;
+      formData.append("file", fs.createReadStream(filePath));
+    }
+
+    // In order to protect against XSRF attacks, because this method accepts multipart/form-data, it has XSRF protection on it.
+    // This means you must submit a header of X-Atlassian-Token: no-check with the request, otherwise it will be blocked.
+    const reqHeadersObj = {
+      method: "POST",
+      body: formData,
+      headers: formData.getHeaders({ "X-Atlassian-Token": "no-check" }),
+    };
+
+    const authHeader = HttpUtils.getAuthHeader(
+      this.jiraSettings.user,
+      this.jiraSettings.password
+    );
+    reqHeadersObj.headers.Authorization = authHeader.headers.Authorization;
+
+    const response = await fetch(_getAttachmentsUrl(issueKey), reqHeadersObj);
+    const isError = "errorMessages" in response;
+    if (isError) {
+      console.log(await response.text());
+      throw `Error uploading attachment ${filePath} to test case ${issueKey}`;
+    }
+  }
+
+  async _loadNextTestCasePage() {
+    if (this.testCases) {
+      this.currentPage++;
+    }
+    if (this.testCases && this.testCases.length < this.pageSize) return true;
+    await this._loadPage(this.currentPage);
+    return this.testCases.length === 0;
+  }
+
+  async _loadPage(pageNumber) {
+    const response = await fetch(
+      this._getTestCaseSearchUrl(pageNumber),
+      HttpUtils.getAuthHeader(
+        this.jiraSettings.user,
+        this.jiraSettings.password
+      )
+    );
+    const isValid = response.status == 200;
+    if (!isValid) {
+      console.log(await response.text());
+      throw "Error retrieving test cases";
+    }
+    this.testCases = (await response.json()).issues;
+  }
+
+  _getMyselfUrl() {
+    return `${this.jiraSettings.url}/rest/api/2/myself`;
+  }
+
+  _getAttachmentsUrl(issueKey) {
+    return `${this.jiraSettings.url}/rest/api/2/issue/${issueKey}/attachments`;
+  }
+
+  _getTestCaseSearchUrl() {
+    return encodeURI(
+      `${this.jiraSettings.url}/rest/api/2/search?jql=project = \"${
+        this.jiraSettings.projectKey
+      }\"&fields=key&startAt=${this.pageSize * this.currentPage}&maxResults=${
+        this.pageSize
+      }`
+    );
+  }
+}
+
+module.exports = TargetJiraApi;
