b8459589f2402df33cd65eb5866738da3ee50b01
Eric Valenzuela <evalenzuela@ctic.us>
2021-06-09 22:19:43 -0700

    TGA-43: reduce code duplication

--- app/index.js
+++ app/index.js
@@ -1,7 +1,6 @@
 const settings = require("../settings.json");
 const SourceJiraApi = require("./sourceJiraApi.js");
-const TargetJiraApi = require("./targetJiraApi.js");
-const TargetTM4JApi = require("./targetTm4jApi.js");
+const ZephyrScaleApi = require("./zephyrScaleApi.js");
 const fs = require("fs");
 
 function deleteFolderRecursive(path) {
@@ -18,59 +17,63 @@ function deleteFolderRecursive(path) {
   }
 }
 
-async function migrateJira() {
-  var sourceJiraApi = new SourceJiraApi(settings.sourceJira);
-  var targetJiraApi = new TargetJiraApi(settings.targetJira);
+function parseOriginalIssueKey(str) {
+  return parseInt(str.replace(/^\D+/g, ""));
+}
 
-  console.log("Validating connection to source Jira...");
-  if (!(await sourceJiraApi.validate())) {
+async function migrateIssues(
+  sourceJiraApi,
+  issueKeyStart,
+  issueKeyEnd,
+  isZephyrScale
+) {
+  var title = isZephyrScale ? "Zephyr Scale" : "Jira";
+  var desc = isZephyrScale ? "test case" : "issue";
+  var zephyrScaleApi = new ZephyrScaleApi(settings.targetJira);
+
+  console.log(`Validating connection to target ${title}...`);
+  if (!(await zephyrScaleApi.validate())) {
     console.log(
-      "Error trying to connect to the source Jira instance. Aborting."
+      `Error trying to connect to the target ${title} instance. Aborting.`
     );
     return;
   }
 
-  console.log("Validating connection to target Jira...");
-  if (!(await targetJiraApi.validate())) {
-    console.log(
-      "Error trying to connect to the target Jira instance. Aborting."
-    );
-    return;
-  }
-
-  console.log("Creating directory to store downloaded attachments...");
-  deleteFolderRecursive("./attachments");
-  fs.mkdirSync("./attachments");
-
-  console.log("All good. Ready to start Jira migration...\n");
+  console.log(`All good. Ready to start ${title} migration...\n`);
 
   var issue;
   let index = 1;
-  while ((issue = await targetJiraApi.getNextTestCase())) {
-    console.log(`#${index++} Issue ${issue.key}:`);
-    var issueKey = issue.key;
+  while ((issue = await zephyrScaleApi.getNextIssue(isZephyrScale))) {
+    var issueKey = zephyrScaleApi.getIssueKey(issue, isZephyrScale);
     if (!issueKey) {
-      console.log("\tNo issue key has been found. Skipping...");
+      console.log(`\tNo ${desc} key has been found. Skipping...`);
       continue;
     }
-    var attachments = await sourceJiraApi.getAttachments(issueKey);
-    if (!attachments.length) {
-      console.log(`\tNo attachments to upload from issue ${issueKey}.`);
-      continue;
+    const key = parseOriginalIssueKey(issueKey);
+    if (key >= issueKeyStart && key <= issueKeyEnd) {
+      console.log(`#${index++} ${desc} ${issue.key}:`);
+      var attachments = await sourceJiraApi.getAttachments(issueKey);
+      if (!attachments.length) {
+        console.log(`\tNo attachments to upload from ${desc} ${issueKey}.`);
+        continue;
+      }
+      console.log(
+        `\tFound ${attachments.length} attachments. Downloading attachments from ${desc} ${issueKey}...`
+      );
+      await sourceJiraApi.downloadAttachments(issueKey, attachments);
+      console.log(`\tUploading attachments to ${desc} ${issue.key}...`);
+      await zephyrScaleApi.uploadAttachments(
+        issueKey,
+        issue.key,
+        isZephyrScale
+      );
     }
-    console.log(
-      `\tFound ${attachments.length} attachments. Downloading attachments from issue ${issueKey}...`
-    );
-    await sourceJiraApi.downloadAttachments(issueKey, attachments);
-    console.log(`\tUploading attachments to issue ${issue.key}...`);
-    await targetJiraApi.uploadAttachments(issueKey, issue.key);
   }
-  console.log("Jira migration complete!");
+  console.log(`${title} migration complete!`);
 }
 
-async function migrateTm4j() {
+async function migrate() {
   var sourceJiraApi = new SourceJiraApi(settings.sourceJira);
-  var targetTm4jApi = new TargetTM4JApi(settings.targetJira);
 
   console.log("Validating connection to source Jira...");
   if (!(await sourceJiraApi.validate())) {
@@ -80,47 +83,29 @@ async function migrateTm4j() {
     return;
   }
 
-  console.log("Validating connection to target Jira...");
-  if (!(await targetTm4jApi.validate())) {
-    console.log(
-      "Error trying to connect to the target Jira instance. Aborting."
-    );
-    return;
-  }
-
   console.log("Creating directory to store downloaded attachments...");
   deleteFolderRecursive("./attachments");
   fs.mkdirSync("./attachments");
 
-  console.log("All good. Ready to start TM4J migration...\n");
+  let issueKeyStart = Number.MIN_SAFE_INTEGER;
+  let issueKeyEnd = Number.MAX_SAFE_INTEGER;
 
-  var testCase;
-  let index = 1;
-  while ((testCase = await targetTm4jApi.getNextTestCase())) {
-    console.log(`#${index++} Test case ${testCase.key}:`);
-    var issueKey = targetTm4jApi.getIssueKey(testCase);
-    if (!issueKey) {
-      console.log("\tNo issue key has been found. Skipping...");
-      continue;
-    }
-    var attachments = await sourceJiraApi.getAttachments(issueKey);
-    if (!attachments.length) {
-      console.log(`\tNo attachments to upload from issue ${issueKey}.`);
-      continue;
-    }
-    console.log(
-      `\tFound ${attachments.length} attachments. Downloading attachments from issue ${issueKey}...`
-    );
-    await sourceJiraApi.downloadAttachments(issueKey, attachments);
-    console.log(`\tUploading attachments to test case ${testCase.key}...`);
-    await targetTm4jApi.uploadAttachments(issueKey, testCase.key);
+  if (settings.sourceJira.issueKeyStart !== "") {
+    issueKeyStart = parseOriginalIssueKey(settings.sourceJira.issueKeyStart);
   }
-  console.log("TM4J migration complete!");
-}
 
-async function migrate() {
-  await migrateJira();
-  await migrateTm4j();
+  if (settings.sourceJira.issueKeyEnd !== "") {
+    issueKeyEnd = parseOriginalIssueKey(settings.sourceJira.issueKeyEnd);
+  }
+
+  var isZephyrScale = true;
+  await migrateIssues(sourceJiraApi, issueKeyStart, issueKeyEnd, isZephyrScale);
+  await migrateIssues(
+    sourceJiraApi,
+    issueKeyStart,
+    issueKeyEnd,
+    !isZephyrScale
+  );
 }
 
 migrate();
